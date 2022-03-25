b004786f0d1839327b937d52c7be7a28bac111e6
Guilherme <guimoltri@gmail.com>
2022-03-25 05:53:42 -0300

    Conclude Lesson 11 Section 1

--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -4,3 +4,9 @@ let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
 
+function sumLaps() {
+    let totatTime = lap1 + lap2 + lap3 
+    console.log(totatTime)
+}
+
+sumLaps()
