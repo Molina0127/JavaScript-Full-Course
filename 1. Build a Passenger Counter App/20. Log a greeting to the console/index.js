b47f1ff95d48f292027bcaa5e1a2fa54b071769e
Guilherme <guimoltri@gmail.com>
2022-03-25 16:47:56 -0300

    Conclude Lesson 21 Section 1

--- 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
+++ 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
@@ -1,7 +1,13 @@
 // Create two variables, name and greeting. The name variable should store your name,
 // and the greeting should store e.g. "Hi, my name is "
 
+let name = 'Guilherme Molina Trindade'
+let greeting = "Hi, my name is "
+
 // Create a third variable, myGreeting, that contatenates the two strings
 // Log myGreeting to the console
 
+let myGreeting = greeting + name
+
+console.log(myGreeting)
 
