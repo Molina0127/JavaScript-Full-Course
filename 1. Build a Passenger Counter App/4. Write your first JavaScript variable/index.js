4b3986aaa0a6f6d945fe8649f0bb81bb222d3f22
Guilherme <guimoltri@gmail.com>
2022-03-25 02:13:49 -0300

    Conclude Lesson 5 of Section 1

--- 1. Build a Passenger Counter App/4. Write your first JavaScript variable/index.js
+++ 1. Build a Passenger Counter App/4. Write your first JavaScript variable/index.js
@@ -4,7 +4,12 @@ let count = 0
 
 console.log(count)
 
+console.log(myAge)
 
 // 1. Create a variable, myAge, and set its value to your age
 
+let myAge = 18
+
 // 2. Log the myAge variable to the console
+
+// console.log(myAge)
