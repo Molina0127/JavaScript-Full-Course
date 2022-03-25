4b92686899fe7a1af687df4baae1e9ab3898100f
Guilherme <guimoltri@gmail.com>
2022-03-25 16:36:03 -0300

    Conclude Lesson 20 Section 1

--- 1. Build a Passenger Counter App/19. Write your first string variable/index.js
+++ 1. Build a Passenger Counter App/19. Write your first string variable/index.js
@@ -1,4 +1,8 @@
-let username = 'per"
+let username = 'per'
+let message = "You have tree new notifications"
 
-console.log(username)
+// Create a variable, messageToUser, that contains the message we have logged
 
+let messageToUser = message + ", " + username + "!"
+
+console.log(messageToUser)
