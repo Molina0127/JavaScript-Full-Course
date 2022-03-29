919008d8b9e767faf62a00c3e6a001e9853b5bc6
Guilherme <guimoltri@gmail.com>
2022-03-29 18:05:40 -0300

    Conclude Lesson 22 Section 1

--- 1. Build a Passenger Counter App/22. Render a welcome message/index.js
+++ 1. Build a Passenger Counter App/22. Render a welcome message/index.js
@@ -1,6 +1,14 @@
 // Grab the welcome-el paragraph and store it in a variable called welcomeEl
 
+let welcomeEl = document.getElementById("welcome-el")
+
 // Create two variables (name & greeting) that contains your name
+
+let name = "Daniele Almeida Martos";
+let greeting = "Seja bem-vindo novamente ";
+
 // and the greeting we want to render on the page
+
+welcomeEl.innerText = greeting + name;
 
 // Render the welcome message using welcomeEl.innerText
