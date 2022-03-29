475d0025a20ef159da1bdc9a524eee40edcb67ef
Guilherme <guimoltri@gmail.com>
2022-03-29 19:28:25 -0300

    Conclude Lesson 23 Section 1

--- 1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
+++ 1. Build a Passenger Counter App/23. Improve the message with string concatenation/index.js
@@ -1,11 +1,15 @@
 let welcomeEl = document.getElementById("welcome-el")
 
-let name = "Per Harald Borgen"
-let greeting = "Welcome back "
+let name = "José Roberto Claudino Ferreira"
+let greeting = "Bem-vindo novamente "
 
 welcomeEl.innerText = greeting + name
 
 // Add an emoji to the end!
 // WRITE YOUR CODE BELOW HERE
 // HINT: count = count + 1
+
+let emoji = " 🙂"
+
+welcomeEl.innerText = welcomeEl.innerText +  emoji
 
