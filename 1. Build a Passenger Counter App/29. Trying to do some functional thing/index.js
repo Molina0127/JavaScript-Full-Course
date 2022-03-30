bf21f7c746499df860937e231198f812d344fa54
Guilherme <guimoltri@gmail.com>
2022-03-30 12:43:54 -0300

    Add function to know what is the taste

--- 1. Build a Passenger Counter App/29. Trying to do some functional thing/index.js
+++ 1. Build a Passenger Counter App/29. Trying to do some functional thing/index.js
@@ -1,24 +1,25 @@
 let count = 0
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let ele = document.getElementById("elementos")
+let select = 0;
 
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+function chooseTaste() {
+    select = ele.options[ele.options.selectedIndex] 
+    console.log(select.value)
+}
+
 function save() {
-    let countStr = count + " - "
+    let countStr = count + " " + select.text + " - "
     saveEl.textContent += countStr
 
     countEl.textContent = 0
     count = 0
 }
 
-function chooseTaste() {
-    let ele = document.getElementById("elementos")
-    let select = ele.options[ele.options.selectedIndex] 
-    console.log(select)
-}
-
-console.log("Let's count people on the subway!")
+console.log("Let's count the number of dindin and them tastes which are ordering by people!")
