91995196f7769aa3f66bd5879d8d6361a73c98ff
Guilherme <guimoltri@gmail.com>
2022-03-31 01:26:52 -0300

    Update Section 29

--- 1. Build a Passenger Counter App/29. Trying to do some functional thing/index.js
+++ 1. Build a Passenger Counter App/29. Trying to do some functional thing/index.js
@@ -3,24 +3,42 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let ele = document.getElementById("elementos")
 let select = 0;
+let i
+
 
 function increment() {
     count += 1
-    countEl.textContent = count + " " + select.text
+    countEl.textContent = count  //+*" " + select.text */
 
+}
+
+function oi() {
+    window.alert("Iihh trocou")
 }
 
 function chooseTaste() {
     select = ele.options[ele.options.selectedIndex] 
     console.log(select.value)
-}
 
-function save() {
     let countStr = count + " " + select.text + " - "
     saveEl.textContent += countStr
+    
+    // array = saveEl.textContent
 
     countEl.textContent = 0
     count = 0
+}
+
+function save() {
+    // let countStr = count + " " + select.text + " - "
+    // saveEl.textContent += countStr
+
+    countEl.textContent = 0
+    count = 0
+}
+
+function clooseOrder() {
+    console.log(saveEl.textContent)
 }
 
 console.log("Let's count the number of dindin and them tastes which are ordering by people!")
