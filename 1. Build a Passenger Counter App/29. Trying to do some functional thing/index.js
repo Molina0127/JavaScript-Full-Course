5479e65e9f65246728e36851b7e3c2cf1d5efd17
Guilherme <guimoltri@gmail.com>
2022-03-31 10:32:46 -0300

    Add some validation to my code

--- 1. Build a Passenger Counter App/29. Trying to do some functional thing/index.js
+++ 1. Build a Passenger Counter App/29. Trying to do some functional thing/index.js
@@ -3,13 +3,19 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let ele = document.getElementById("elementos")
 let select = 0;
-let i
+let a = false;
+let b;
 
 
 function increment() {
+    
     count += 1
-    countEl.textContent = count  //+*" " + select.text */
-
+    countEl.textContent = count //+*" " + select.text */
+    a = true
+    
+    // window.alert("Escolha um sabor")
+    
+    
 }
 
 function oi() {
@@ -18,17 +24,25 @@ function oi() {
 
 function chooseTaste() {
     select = ele.options[ele.options.selectedIndex] 
-    console.log(select.value)
-
-    if (count != 0) {
-        let countStr = count + " " + select.text + " - "
-        saveEl.textContent += countStr
-
-        countEl.textContent = 0
-        count = 0
-    }
-
     
+    let resultado = confirm(`Você tem certeza que quer um Dindin do sabor ${select.text} ?`)
+    
+    if (resultado == true){
+        if (select.value != 0 && count != 0) {
+            let countStr = count + " " + select.text + " - "
+            saveEl.textContent += countStr
+    
+            countEl.textContent = 0
+            count = 0
+        }
+    }
+    else {
+        count = 0
+        ele.options.selectedIndex = 0
+        select = ele.options.selectedIndex    
+    }
+    // console.log(select)
+
 }
 
 function save() {
