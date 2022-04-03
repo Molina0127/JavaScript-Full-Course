44c5537ec86b6c0046c5a689fb5cf62159d8347d
Guilherme <guimoltri@gmail.com>
2022-04-02 23:32:58 -0300

    Change Function's name

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -2,7 +2,7 @@ let name = "Linda"
 let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
-function hello() {
+function greetLinda() {
     console.log(greeting + ", " + name + "!")
 }
 
