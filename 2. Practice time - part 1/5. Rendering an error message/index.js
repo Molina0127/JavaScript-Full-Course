4f7f80f69bfe8f767ca79b5cf403c7fadb8394cd
Guilherme <guimoltri@gmail.com>
2022-04-06 19:32:00 -0300

    Start to do the Lesson 5 Section 2

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -1,5 +1,9 @@
 // When the user clicks the purchase button, render out
-// "Something went wrong, please try again" in the paragraph
-// that has the id="error".
 
+
+// "Something went wrong, please try again" in the paragraph
+
+
+// that has the id="error".
+document.getElementById("error")
 
