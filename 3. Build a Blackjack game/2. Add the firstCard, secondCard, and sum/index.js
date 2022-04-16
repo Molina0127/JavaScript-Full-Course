231902a045b00957a65c8494e9d536bf7ac42b02
Guilherme <guimoltri@gmail.com>
2022-04-15 23:40:17 -0300

    Improve the code

--- 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
@@ -1,10 +1,7 @@
 // 1. Create two variables, firstCard and secondCard.
-let firstCard;
-let secondCard;
-
 // Set their values to a random number between 2-11
-firstCard = 2;
-secondCard = 11;
+let firstCard = 2
+let secondCard = 11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard;
