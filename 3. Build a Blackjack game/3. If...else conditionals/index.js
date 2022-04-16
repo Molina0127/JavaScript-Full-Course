788fa0a5509b81bd27f720848529043b617c4043
Guilherme <guimoltri@gmail.com>
2022-04-15 23:54:22 -0300

    Standart code like tutorial

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,9 +2,12 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
-if (sum <= 21) {
-
+if (sum < 21) {
+    console.log("Do you want to draw a new car?")
+}
+else if (sum === 21) {
+    console.log("Wohoo! You've got a Blackjack!")
 }
 else if (sum > 21) {
-    
+    console.log("You're out of the game!")
 }
