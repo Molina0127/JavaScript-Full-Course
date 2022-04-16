d30617416b865cdaf026d111676bf0684384de2a
Guilherme <guimoltri@gmail.com>
2022-04-15 23:38:13 -0300

    First try to solve the challenge

--- 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
@@ -1,4 +1,12 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
+let firstCard;
+let secondCard;
+
 // Set their values to a random number between 2-11
+firstCard = 2;
+secondCard = 11;
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard;
+
+console.log(sum)
