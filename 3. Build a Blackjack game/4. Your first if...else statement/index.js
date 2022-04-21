f671729cf12dea4913e2e56147a5063ea58aa8e1
Guilherme <guimoltri@gmail.com>
2022-04-20 22:23:27 -0300

    Initial solve of lesson 4

--- 3. Build a Blackjack game/4. Your first if...else statement/index.js
+++ 3. Build a Blackjack game/4. Your first if...else statement/index.js
@@ -3,6 +3,13 @@
 
 let age = 22
 
+if (age > 21) {
+    console.log("You can not enter the club!")
+}
+else {
+    console.log("Welcome!")
+}
+
 // if less than 21 -> "You can not enter the club!"
 // else            -> "Welcome!"
 
