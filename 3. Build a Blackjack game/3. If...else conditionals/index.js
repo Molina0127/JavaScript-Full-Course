269c73843d50009556381175a38943897ee5a63b
Guilherme <guimoltri@gmail.com>
2022-04-22 23:58:16 -0300

    Create some test

--- 3. Build a Blackjack game/3. If...else conditionals/index.js
+++ 3. Build a Blackjack game/3. If...else conditionals/index.js
@@ -2,15 +2,19 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 
+function teste() {
+    if (sum < 21) {
+        console.log("Do you want to draw a new car?")
+    }
+    else if (sum === 21) {
+        console.log("Wohoo! You've got a Blackjack!")
+    }
+    else if (sum > 21) {
+        console.log("You're out of the game!")
+    }
+}
 
-if (sum < 21) {
-    console.log("Do you want to draw a new car?")
-}
-else if (sum === 21) {
-    console.log("Wohoo! You've got a Blackjack!")
-}
-else if (sum > 21) {
-    console.log("You're out of the game!")
-}
+teste()
+
 
 
